add08dacbee4c98758d758e3b132647a6c6c802e
nathanstephenson <nathans261@gmail.com>
2021-10-19 14:59:30 +0100

    008: added IO interface and implementation

---
+++ src/main/java/org/nathan/vendingmachine/ui/UserIO.java
@@ -0,0 +1,15 @@
+package org.nathan.vendingmachine.ui;
+
+import org.nathan.vendingmachine.service.Currency;
+
+public interface UserIO {
+    void print(String message);
+
+    String readString(String prompt);
+
+    int readInt(String prompt);
+
+    int readInt(String prompt, int min, int max);
+
+    Currency readCurrency(String prompt);
+}
---
+++ src/main/java/org/nathan/vendingmachine/ui/UserIOConsoleImpl.java
@@ -0,0 +1,59 @@
+package org.nathan.vendingmachine.ui;
+
+import org.nathan.vendingmachine.service.Currency;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class UserIOConsoleImpl implements UserIO {
+    private Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public String readString(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+
+    @Override
+    public int readInt(String prompt) {
+        System.out.println(prompt);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    @Override
+    public int readInt(String prompt, int min, int max) {
+        boolean inRange;
+        int input;
+        do {
+            System.out.println(prompt);
+            input = Integer.parseInt(scanner.nextLine());
+            inRange = input >= min && input <= max;
+        } while (!inRange);
+        return input;
+    }
+
+    @Override
+    public Currency readCurrency(String prompt) {
+        boolean validInput = false;
+        System.out.println(prompt);
+        for (Currency c : Currency.values()) {
+            System.out.println(c.name());
+        }
+        String input;
+        do {
+            input = scanner.nextLine().toUpperCase(Locale.ROOT);
+            for (Currency c : Currency.values()) {
+                validInput = c.name().equals(input) || validInput;
+            }
+            System.out.println(validInput ? "" : "Please choose a valid coin to insert");
+        } while (!validInput);
+        return Currency.valueOf(input);
+    }
+
+
+}
