0aa86cade9bab4018ca4c03abef96e9b295a05f0
nathanstephenson <nathans261@gmail.com>
2021-10-21 16:00:37 +0100

    019: App added as entry point

--- src/main/java/org/nathan/vendingmachine/App.java
+++ src/main/java/org/nathan/vendingmachine/App.java
@@ -1,4 +1,19 @@
 package org.nathan.vendingmachine;
 
+import org.nathan.vendingmachine.controller.VendingMachineController;
+import org.nathan.vendingmachine.dao.AuditDaoException;
+import org.nathan.vendingmachine.dao.AuditDaoImpl;
+import org.nathan.vendingmachine.dao.VendingMachineDaoException;
+import org.nathan.vendingmachine.dao.VendingMachineDaoImpl;
+import org.nathan.vendingmachine.service.InsufficientFundsException;
+import org.nathan.vendingmachine.service.VendingMachineServiceImpl;
+import org.nathan.vendingmachine.ui.UserIOConsoleImpl;
+import org.nathan.vendingmachine.ui.VendingMachineView;
+
 public class App {
+    public static void main(String[] args) throws AuditDaoException, VendingMachineDaoException, InsufficientFundsException {
+        VendingMachineController controller = new VendingMachineController(
+                new VendingMachineServiceImpl(new VendingMachineDaoImpl(), new AuditDaoImpl("AuditLog")), new VendingMachineView(new UserIOConsoleImpl()));
+        controller.run();
+    }
 }
