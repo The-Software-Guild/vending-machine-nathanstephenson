91089d107c233ba401b2565228faf1a50393cb3e
nathanstephenson <nathans261@gmail.com>
2021-10-22 11:21:41 +0100

    added test file and updated pom

---
+++ src/test/java/org/nathan/vendingmachine/service/VendingMachineServiceLayerTest.java
@@ -0,0 +1,66 @@
+package org.nathan.vendingmachine.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VendingMachineServiceLayerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSnack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSnacks() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addSnack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeSnack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void editSnack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loadStock() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveStock() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validateSnackExists() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testValidateSnackExists() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validateSnackSelection() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validateFilename() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fundsValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sufficientFunds() {
+    }
+}
